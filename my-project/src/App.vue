32e63322182894366d2ca935f3ea5f00be71a8ec
liaoshanggang <787887060@qq.com>
2018-07-09 22:00:43 +0800

    firstcomponent

---
+++ my-project/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png"><br>
+    <router-link to="/view1">Go to view1</router-link>
+    <router-link to="/view2">Go to view2</router-link>
+    <router-link to="/others">Go to others</router-link>
+<!-- 路由出口 -->
+<!-- 路由匹配到的组件将渲染在这里 -->
+<!--当你访问对应的路由 时，router-view 就被对应组件替换了。-->
+    <router-view/>
+    <!--父组件App.vue的msg:<br>-->
+    <h1>{{msg}}</h1>
+　　<firstcomponent></firstcomponent>
+  </div>
+</template>
+<script>
+import firstcomponent from './components/firstcomponent.vue'
+export default {
+  // name: 'App',
+  data () {
+　　　　return {
+　　　　　　msg: 'Welcome to Your Vue.js App'
+　　　　}
+　},
+  components: { 
+    firstcomponent 
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ my-project/src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import View1 from '@/views/view1'
+import View2 from '@/views/view2'
+
+Vue.use(Router)
+
+export default new Router({
+  linkActiveClass: 'active',
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+        path: '/view1',
+        component: View1
+    }, {
+        path: '/view2',
+        component: View2
+    }, {
+        path: '/*',
+        component: HelloWorld
+    }
+  ]
+})
